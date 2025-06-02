5c12a63766c89c5b298d68b066c2c017cc0c181a
Leandro Blandi <leandronblandi@gmail.com>
2025-06-01 22:16:56 -0300

    feat: Confirmación de propiedad/experiencia

--- src/app/home/acommodation-detail/acommodation-detail.component.ts
+++ src/app/home/acommodation-detail/acommodation-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Alojamiento } from '../../core/models/alojamiento.model';
 import { AlojamientoService } from '../../core/services/alojamiento.service';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-acommodation-detail',
@@ -13,13 +14,16 @@ import { ActivatedRoute } from '@angular/router';
 export class AcommodationDetailComponent implements OnInit {
   public alojamiento : Alojamiento | null = null;
 
-  constructor(private alojamientoService: AlojamientoService,private route: ActivatedRoute) { }
+  constructor(private alojamientoService: AlojamientoService, private route: ActivatedRoute, private toastService: ToastrService) { }
 
   ngOnInit() {
 
     this.route.params.subscribe((params: any) => {
       this.alojamiento = this.alojamientoService.getAlojamientoById(parseInt(params["id"]));
-      console.log(this.alojamiento);
     });
+  }
+
+  showConfirm() {
+    this.toastService.success("En los próximos minutos, recibirás un mail con la confirmación de la reserva", "Reservaste con éxito esta propiedad");
   }
 }
--- src/app/home/experience-detail/experience-detail.component.ts
+++ src/app/home/experience-detail/experience-detail.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Experiencia } from '../../core/models/experiencia.model';
 import { ExperienciaService } from '../../core/services/experiencia.service';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-experience-detail',
@@ -13,7 +14,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ExperienceDetailComponent {
   public experiencia : Experiencia | null = null;
 
-  constructor(private experienciaService: ExperienciaService, private route: ActivatedRoute) { }
+  constructor(private experienciaService: ExperienciaService, private route: ActivatedRoute, private toastService: ToastrService) { }
 
   ngOnInit() {
 
@@ -21,5 +22,9 @@ export class ExperienceDetailComponent {
       this.experiencia = this.experienciaService.getExperienciaById(parseInt(params["id"]));
       console.log(this.experiencia);
     });
+  }
+
+  showConfirm() {
+    this.toastService.success("En los próximos minutos, recibirás un mail con la confirmación de la reserva", "Reservaste con éxito un cupo de esta experiencia");
   }
 }
